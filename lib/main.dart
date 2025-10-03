19bc300c5e6536c94beca142fbaa5b6208d18fd8
AKS-2003 <akshaya@farmonaut.com>
2025-10-03 21:19:54 +0530

    wip:-

--- lib/main.dart
+++ lib/main.dart
@@ -176,6 +176,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Polismitr',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
